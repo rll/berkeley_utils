96325129367e0da7620307b87101572cf507b82d
Stephen Miller <sdmiller@eecs.berkeley.edu>
2011-06-01 09:35:40 +0000

    Added topic utilities

--- rll_utils/src/rll_utils/RosUtils.py
+++ rll_utils/src/rll_utils/RosUtils.py
@@ -17,3 +17,26 @@ def call_service(service_name, service_type, *non_keyword_args, **keyword_args):
     except rospy.ServiceException,e:
         rospy.loginfo("Service Call Failed: %s"%str(e))
         return None
+
+def get_next_message(topic,message_type,timeout=None):
+    responses = []
+    sub = rospy.Subscriber(topic,message_type,lambda msg: responses.append(msg))
+    start = rospy.Time.now()
+    if timeout:
+        finish = rospy.Time.now() + rospy.Duration(timeout)
+    while len(responses) == 0:
+        rospy.sleep(0.1)
+        if timeout:
+            if rospy.Time.now() >= finish:
+                responses.append(None)     
+    sub.unregister()
+    output = responses[0]
+    for val in list(responses):
+        responses.remove(val)
+    return output
+    
+    
+def call_and_response(output_topic,output_type,output_message,input_topic,input_type,timeout=None):
+    pub = rospy.Publisher(output_topic,output_type)
+    pub.publish(output_message)
+    return get_next_message(input_topic,input_type,timeout)
