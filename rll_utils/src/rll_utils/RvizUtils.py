3e1cc2fa9e196fdd72ae0ac469f2151f6c9fa7fb
Ibrahim Awwal <ibrahim.awwal@berkeley.edu>
2011-07-24 06:03:56 +0000

    Allow changing the orientation of the marker being published by place_marker.

--- rll_utils/src/rll_utils/RvizUtils.py
+++ rll_utils/src/rll_utils/RvizUtils.py
@@ -3,8 +3,9 @@ import roslib
 roslib.load_manifest("rll_utils")
 import rospy
 from visualization_msgs.msg import Marker
+from geometry_msgs.msg import *
 
-def place_marker(point, pub, _id=0, _type = Marker.CUBE, r = 0, g = 1, b = 0, xscale = .03, yscale=.03, zscale = .03):
+def place_marker(point, pub, _id=0, _type = Marker.CUBE, r = 0, g = 1, b = 0, xscale = .03, yscale=.03, zscale = .03, orientation = Quaternion(0,0,0,1)):
     """
     Publishes a visualization marker for Rviz. The publisher needs to be supplied just because I've noticed
     that sometimes when you try to make the publisher in this method, it doesn't work for whatever reason.
@@ -13,7 +14,7 @@ def place_marker(point, pub, _id=0, _type = Marker.CUBE, r = 0, g = 1, b = 0, xs
     marker.header.frame_id = point.header.frame_id
     marker.header.stamp = rospy.Time.now()
     marker.pose.position = point.point
-    marker.pose.orientation.w = 1
+    marker.pose.orientation = orientation
     marker.scale.x = xscale
     marker.scale.y = yscale
     marker.scale.z = zscale
